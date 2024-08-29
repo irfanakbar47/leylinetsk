db16a36eb5322cda5649b048ae866acc8ce700e1
Irfan <irfanakbar47@gmail.com>
2024-08-29 18:05:30 +0000

    adding root health and metrics endpoint to test

--- test_main.py
+++ test_main.py
@@ -3,7 +3,22 @@ from main import app
 
 client = TestClient(app)
 
+def test_root_endpoint():
+    response = client.get("/")
+    assert response.status_code == 200
+    json_response = response.json()
+    assert "date" in json_response
+    assert "version" in json_response
+    assert "kubernetes" in json_response
+
+
 def test_health_check():
     response = client.get("/health")
     assert response.status_code == 200
     assert response.json() == {"status": "healthy"}
+
+def test_metrics_endpoint():
+    response = client.get("/metrics")
+    assert response.status_code == 200
+    assert "promhttp_metric_handler_requests_total" in response.text
+
