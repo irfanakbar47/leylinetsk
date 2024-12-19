21641d8bce00cae93696fbc94f5695d5a5aeb83f
irfanakbar47 <38893986+irfanakbar47@users.noreply.github.com>
2024-12-20 01:24:49 +0500

    Update main.py

--- main.py
+++ main.py
@@ -195,4 +195,4 @@ logger = logging.getLogger(__name__)
 
 if __name__ == "__main__":
     import uvicorn
-    uvicorn.run(app, host="0.0.0.0", port=3000, log_level="info")
+    uvicorn.run(app, host="0.0.0.0", port=3001, log_level="info")
